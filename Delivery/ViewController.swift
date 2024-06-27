bc8eac64f0027805fcbb79bfb4e5359dcd5f557c
Nikita Shmatov <pravdomir42@gmail.com>
2024-06-27 18:46:39 +0300

    Initial Commit

---
+++ Delivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Delivery
+//
+//  Created by Shmatov Nikita on 27.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
